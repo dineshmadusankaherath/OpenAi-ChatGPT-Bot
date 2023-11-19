71a91c245c0d1e15867eb8628b5674333647d6a1
dineshmadusankaherath <143239776+dineshmadusankaherath@users.noreply.github.com>
2023-11-19 07:48:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ global.BOT_PRIVATE = false // if you want use bot privet
 global.AUTO_CHATGPT = false // if you want auto ai bot mod
 global.READ_MESSAGE = false // if you want read message from bot
 global.ONLY_INBOX = false // Only inbox mod ( only pm ) use
-global.ONLY_GROUPS = false // Only Groups Use
+global.ONLY_GROUPS = true // Only Groups Use
 
 
 /*
@@ -21,14 +21,14 @@ global.OWNER_NUMBER = ['94781536595'] //Owner Number
 global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers Like a Sudo Numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 global.IMAGE_QUALITY = 'low' // Available Quality Are => low | mediam | high
-global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴𝙽-𝙰𝙸(𝙲𝙷𝙰𝚃𝙶𝙿𝚃) 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃*'
+global.CAPTION = '*qᴜᴇᴇɴ ꜱᴀᴅᴜ ᴜᴩᴅᴀᴛᴇ -ᵐʳ ᴅɪɴᴇꜱʜ*'
 
 /*
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
 global.AI_PASSWORD = 'King-nima-gpt-bot'
-global.SESSION_ID=''
+global.SESSION_ID='elisa_md@;;;BY0TxSJZ#HUDcsvMcqznXwtR4TgnZgOdtpvemu5imwbXHyJhNcA0'
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
 */
